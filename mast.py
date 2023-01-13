d816b455dab9534da7d9f863410ae1f830cd519f
Blair Fitzhugh <bfitz1@umbc.edu>
2023-01-12 21:21:45 -0500

    Rework AST to use dataclasses

---
+++ mast.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+from typing import Any
+
+# Remark: This implementation bothers me, but I'm not sure how to fix it.
+# - Plain classes mean a lot of boilerplate. I'd like to avoid that if possible.
+# - Dataclasses are more compact, but writing type hints doesn't feel great.
+# - Why not just stick to values (list, tuples and dicts)? I dunno, in too deep I guess.
+
+@dataclass
+class PrefixExpression:
+    operator: str
+    right: Any
+
+@dataclass
+class InfixExpression:
+    left: Any
+    operator: str
+    right: Any
+
+@dataclass
+class BlockStatement:
+    statements: Any
+
+@dataclass
+class IfExpression:
+    condition: Any
+    consequence: BlockStatement
+    alternative: BlockStatement
+
+@dataclass
+class Identifier:
+    value: str
+
+@dataclass
+class IntegerLiteral:
+    value: int
+
+@dataclass
+class Boolean:
+    value: bool
+
+@dataclass
+class FunctionLiteral:
+    parameters: list[Identifier]
+    body: BlockStatement
+
+@dataclass
+class CallExpression:
+    function: Identifier | FunctionLiteral
+    arguments: list[Any]
+
+@dataclass
+class LetStatement:
+    identifier: str
+    expr: Any
+
+@dataclass
+class ReturnStatement:
+    expr: Any
+
+@dataclass
+class ExpressionStatement:
+    expr: Any
+
+@dataclass
+class Program:
+    statements: list[Any]
+
+    def __iter__(self):
+        yield from self.statements
+    
+    def __repr__(self):
+        return f"Program({self.statements})"
